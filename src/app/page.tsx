76ee255f806a8ddabe40a49caf3984116a617449
mazeem77 <mazeem8856@gmail.com>
2024-10-01 16:19:00 +0500

    Lint errors removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,12 @@
 "use client";
-import dynamic from "next/dynamic";
-import { HoverEffect } from "~/components/ui/card-hover-effect";
-import { Controls, Player } from "@lottiefiles/react-lottie-player";
-import BooksImage from "../../public/book.json";
-import image1 from "../../public/homePage/Textbooks.jpg";
-import image2 from "../../public/homePage/etextbooks.png";
-import image3 from "../../public/homePage/Books-Banner.jpg";
-import image4 from "../../public/homePage/UOW_Merch.jpg";
-import image5 from "../../public/homePage/Graduation.jpg";
-import image6 from "../../public/homePage/Gifts.jpg";
-import ContactImage from "../../public/homePage/about-us-side.jpg";
-import Image, { StaticImageData } from "next/image";
-import Link from "next/link";
 import CategoriesSideBar from "~/components/ui-components/CategoriesSideBar"; // Importing Sidebar component
 
-interface itemsType {
-  title: string;
-  description: string;
-  link: string;
-  image_path: StaticImageData;
-}
-
 const HomePage = () => {
-  const projects: itemsType[] = [
-    {
-      title: "Textbooks",
-      description:
-        "Engineering, Science, Business and Arts, Social Sciences & Humanities.",
-      link: "books?detail=Engineering and Information Sciences EIS",
-      image_path: image1,
-    },
-    {
-      title: "E-Textbooks",
-      description:
-        "Read on the device that works for you in Bookshelf, the most used textbook reading app loaded with built-in, interactive features.",
-      link: "books?detail=Engineering and Information Sciences EIS",
-      image_path: image2,
-    },
-    {
-      title: "Books",
-      description:
-        " GENERAL FICTION,NON-FICTION, CHILDREN'S BOOKS and TRAVEL GUIDES",
-      link: "books?detail=GENERAL FICTION",
-      image_path: image3,
-    },
-    {
-      title: "UOW Merch",
-      description:
-        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-      link: "cloths?detail=MERCHANDISE",
-      image_path: image4,
-    },
-    {
-      title: "Clothing",
-      description:
-        "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-      link: "cloths?detail=CLOTHING",
-      image_path: image5,
-    },
-    {
-      title: "Gifts",
-      description: " ERSTWILDER, INDIGENOUS ART MERCH and  IMAGINE HOMEWARES",
-      link: "gifts?detail=ERSTWILDER",
-      image_path: image6,
-    },
-  ];
-
   return (
     <div className="flex">
-       <div className="hidden lg:block "> 
-          <CategoriesSideBar />
-        </div>
+      <div className="hidden lg:block ">
+        <CategoriesSideBar />
+      </div>
 
       {/* Main content area to the right of the sidebar */}
       {/* <main className="flex-1 p-6">
--- types/tailwindcss.d.ts
+++ types/tailwindcss.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'tailwindcss/lib/util/flattenColorPalette' {
   function flattenColorPalette(colors: any): any;
   export default flattenColorPalette;
