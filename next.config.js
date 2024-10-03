46ac1047948964c1c391f4099bb368948a272d5b
mazeem77 <mazeem8856@gmail.com>
2024-10-03 13:29:56 +0500

    small change

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
