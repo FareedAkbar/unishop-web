749eb4b4a5592acc00993863075fb9e063866778
mazeem77 <mazeem8856@gmail.com>
2024-10-03 13:39:01 +0500

    Saved the day

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,9 +4,9 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex flex-1">
+    <>
       <Header />
       {children}
-    </div>
+    </>
   );
 }
