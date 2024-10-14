36430bbb85456855cf08fd34740fc0f94c69a7a0
FareedAkbar <69406456+FareedAkbar@users.noreply.github.com>
2024-10-14 10:36:55 +0500

    next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
