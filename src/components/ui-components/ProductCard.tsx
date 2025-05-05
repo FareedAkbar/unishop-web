71e6d4773dd2bf2e14bb6a74afff7267863ed482
iqra-shahzadi <iqrashahzadi828@gmail.com>
2025-05-05 10:52:53 +0500

    tags in card

--- src/components/ui-components/ProductCard.tsx
+++ src/components/ui-components/ProductCard.tsx
@@ -49,21 +49,18 @@ const ProductCard = ({
 
   const tagNames: string[] = matchingTags?.map((tag) => tag?.tag_name) ?? [];
 
-
   const manageUsage = () => {
     if (product?.book_usages && product?.book_usages.length > 0) {
       return product.book_usages
         .filter((usage) => usage.default_semester === 1)
         .map((usage) => ({
-          type_id: usage.type_id,  // Assuming `type_id` exists
+          type_id: usage.type_id, // Assuming `type_id` exists
           subject_name: usage.subject_name,
-          subject_code: usage.subject_code // Assuming `subject_name` exists
+          subject_code: usage.subject_code, // Assuming `subject_name` exists
         }));
     }
     return [];
   };
-
-
 
   return (
     <div className="group relative flex w-44 flex-shrink-0 grow-0 flex-col rounded-md border p-2 transition-transform duration-300 hover:scale-105 sm:w-64 md:w-64 lg:w-72">
@@ -89,13 +86,14 @@ const ProductCard = ({
           src={
             product?.object_path
               ? `https://ipos-storage.s3.amazonaws.com/${product.object_path}`
-              : product?.media && product?.media.length > 0 ? `https://ipos-storage.s3.amazonaws.com/${product.media[0]?.object_path}`
+              : product?.media && product?.media.length > 0
+                ? `https://ipos-storage.s3.amazonaws.com/${product.media[0]?.object_path}`
                 : "/assets/images/products/product.png"
           }
           alt={product?.SKU_title ?? ""}
           width={1000}
           height={1000}
-          className="cursor-pointer h-32 object-contain transition-transform duration-300 group-hover:scale-110 lg:h-56 lg:w-56" // Scale on hover
+          className="h-32 cursor-pointer object-contain transition-transform duration-300 group-hover:scale-110 lg:h-56 lg:w-56" // Scale on hover
         />
         <div className="absolute right-5 top-2 flex">
           {showButton && !showAddToCart && (
@@ -111,7 +109,7 @@ const ProductCard = ({
             className="rounded-full border-none bg-transparent bg-white p-0.5 text-sm hover:text-red-500 dark:bg-slate-400 sm:p-1 sm:text-xl"
           >
             {product?.item_id &&
-              favItems?.some((favItem) => favItem.item_id === product.item_id) ? (
+            favItems?.some((favItem) => favItem.item_id === product.item_id) ? (
               <AiFillHeart color="red" />
             ) : (
               <AiOutlineHeart />
@@ -126,12 +124,13 @@ const ProductCard = ({
         </div>
 
         {showButton &&
-          (product?.variations?.[0]?.items_variable_items_sale_price ??
-            product?.item_sale_price) ? (
+        (product?.variations?.[0]?.items_variable_items_sale_price ??
+          product?.item_sale_price) ? (
           <button
             onClick={!showAddToCart ? onRemoveFromCart : onAddToCart}
-            className={`absolute bottom-0 z-5 mt-4 w-full rounded-b-sm py-1 text-xs text-white transition-colors sm:py-2 sm:text-sm ${!showAddToCart ? "bg-red-500" : "bg-black"
-              }`}
+            className={`z-5 absolute bottom-0 mt-4 w-full rounded-b-sm py-1 text-xs text-white transition-colors sm:py-2 sm:text-sm ${
+              !showAddToCart ? "bg-red-500" : "bg-black"
+            }`}
           >
             {!showAddToCart ? "Remove From Cart" : "Add To Cart"}
           </button>
@@ -139,13 +138,14 @@ const ProductCard = ({
           ""
         )}
       </div>
-      <span className="mt-2 font-semibold sm:mt-4 mb-1" title={product?.item_name}>
+      <span
+        className="mb-1 mt-2 font-semibold sm:mt-4"
+        title={product?.item_name}
+      >
         {product?.item_name}
       </span>
       {product?.SKU ? (
-        <span className="truncate text-xs">
-          SKU: {product?.SKU}
-        </span>
+        <span className="truncate text-xs">SKU: {product?.SKU}</span>
       ) : (
         ""
       )}
@@ -158,36 +158,30 @@ const ProductCard = ({
       )} */}
 
       {product?.book_id && product?.food_id == null ? (
-        <span className="truncate text-sm">
+        <div className="flex flex-wrap gap-2 text-sm">
           {manageUsage().length > 0 ? (
-            <>
-
-              {manageUsage().map((item, index) => {
-                const matchedType = textbookType?.find((t) => t.item_book_type_id === item.type_id); // Find the matching type
-                return (
-                  <div key={`usage-${item.subject_code}-${index}-name-type`} className="mb-1">
-                    <small
-                      key={`usage-${item.subject_code}-${index}-name`}
-                      className="bg-red-500  text-gray-100  px-2 py-1 rounded mr-1"
-                    >
-                      {item.subject_name} {item.subject_code} {/* Display type_name or fallback */}
-                    </small>
-                    <small
-                      key={`usage-${item.subject_code}-${index}-type`}
-                      className="bg-yellow-200 dark:bg-yellow-500  text-black  px-2 py-1 rounded mr-1"
-                    >
-                      {matchedType?.type_name ?? ""} {/* Display type_name or fallback */}
-                    </small>
-                  </div>
-                );
-
-              })}
-            </>
+            manageUsage().map((item, index) => {
+              const matchedType = textbookType?.find(
+                (t) => t.item_book_type_id === item.type_id,
+              );
+              return (
+                <div
+                  key={`usage-${item.subject_code}-${index}-pair`}
+                  className="flex flex-col items-start gap-1 sm:flex-row"
+                >
+                  <span className="inline-block w-fit rounded bg-red-500 px-2 py-1 text-xs text-white">
+                    {item.subject_name} {item.subject_code}
+                  </span>
+                  <span className="inline-block w-fit rounded bg-yellow-200 px-2 py-1 text-xs text-black dark:bg-yellow-500">
+                    {matchedType?.type_name ?? ""}
+                  </span>
+                </div>
+              );
+            })
           ) : (
-            "Textbook not used this session"
+            <p>Textbook not used this session</p>
           )}
-
-        </span>
+        </div>
       ) : (
         ""
       )}
